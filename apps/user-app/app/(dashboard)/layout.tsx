52ae5ea57fd76d4b9fb066e661dedcf59a4e7406
Aaditya Malani <aadityamalanii15@gmail.com>
2024-07-12 15:35:11 +0530

    ci/cd pipeline coming up

--- apps/user-app/app/(dashboard)/layout.tsx
+++ apps/user-app/app/(dashboard)/layout.tsx
@@ -12,6 +12,7 @@ export default function Layout({
                 <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
                 <SidebarItem href={"/transfer"} icon={<TransferIcon />} title="Transfer" />
                 <SidebarItem href={"/transactions"} icon={<TransactionsIcon />} title="Transactions" />
+                <SidebarItem href={"/p2p"} icon={<TransferIcon />} title="P2P" />
             </div>
         </div>
             {children}
