fc872f4a07fa4f541e09ec1f99d6c962e9e0096e
Aaditya Malani <aadityamalanii15@gmail.com>
2024-07-17 12:39:12 +0530

    type error fixed

--- apps/user-app/app/lib/p2pTransfer.tsx
+++ apps/user-app/app/lib/p2pTransfer.tsx
@@ -1,6 +1,5 @@
 "use server";
 import { getServerSession } from "next-auth";
-// import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 import { authOptions } from "./auth";
 
@@ -23,32 +22,41 @@ export async function p2pTransfer(to: string, amount: number) {
       message: "User not found",
     };
   }
-  await prisma.$transaction(async (tx) => {
-    await tx.$queryRaw`SELECT * FROM "Balance" WHERE id = ${Number(from)} FOR UPDATE`;
-    const fromBalance = await tx.balance.findUnique({
-      where: { userId: Number(from) },
-    });
-    if (!fromBalance || fromBalance.amount < amount) {
-      throw new Error("Insufficient funds");
-    }
+  try {
+    await prisma.$transaction(async (tx) => {
+      await tx.$queryRaw`SELECT * FROM "Balance" WHERE id = ${Number(from)} FOR UPDATE`;
+      const fromBalance = await tx.balance.findUnique({
+        where: { userId: Number(from) },
+      });
+      if (!fromBalance || fromBalance.amount < amount) {
+        throw new Error("Insufficient funds");
+      }
 
-    await tx.balance.update({
-      where: { userId: Number(from) },
-      data: { amount: { decrement: amount } },
-    });
+      await tx.balance.update({
+        where: { userId: Number(from) },
+        data: { amount: { decrement: amount } },
+      });
 
-    await tx.balance.update({
-      where: { userId: toUser.id },
-      data: { amount: { increment: amount } },
-    });
+      await tx.balance.update({
+        where: { userId: toUser.id },
+        data: { amount: { increment: amount } },
+      });
 
-    await tx.p2pTransfer.create({
-      data: {
-        fromUserId: Number(from),
-        toUserId: toUser.id,
-        amount,
-        timestamp: new Date(),
-      },
+      await tx.p2pTransfer.create({
+        data: {
+          fromUserId: Number(from),
+          toUserId: toUser.id,
+          amount,
+          timestamp: new Date(),
+        },
+      });
     });
-  });
+    return {
+      message: "Transfer successful",
+    };
+  } catch (error) {
+    return {
+      message: error.message,
+    };
+  }
 }
