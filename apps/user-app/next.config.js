83474df2a5019ed566b6758d63333bc56ef5dc2a
Aaditya Malani <aadityamalanii15@gmail.com>
2024-07-20 23:43:32 +0530

    issues fixed

--- apps/user-app/next.config.js
+++ apps/user-app/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
 module.exports = {
+  ...nextConfig,
   transpilePackages: ["@repo/ui"],
 };
